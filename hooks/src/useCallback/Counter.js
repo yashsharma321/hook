c154abbbc598b4944d4e3b1398153e9d154c449c
Yash Sharma <yash.arkedia@gmail.com>
2023-11-26 08:45:31 +0000

    useCallback Test

--- hooks/src/useCallback/Counter.js
+++ hooks/src/useCallback/Counter.js
@@ -3,19 +3,22 @@ import Child from "./Child";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Ram");
   console.log("render");
 
   const callback = useCallback(() => {
     //calculation
-  }, []);
+  }, [name]);
 
   return (
     <div className="border-bottom">
       <h1>useCallback Hook</h1>
       <h2>{count}</h2>
+      <h2>{name}</h2>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         Increase Count
       </button>
+      <button onClick={() => setName("Jai Shree Ram")}>update name</button>
       <Child callback={callback} />
     </div>
   );
